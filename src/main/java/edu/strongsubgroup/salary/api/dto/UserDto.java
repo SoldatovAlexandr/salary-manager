752ae89a9f33acf571d628f2697973c843e2539e
akorzh <alexey.korzh.kas@gmail.com>
2021-10-24 20:26:05 +0600

    SALSYS-7:Подключение spring-security

---
+++ src/main/java/edu/strongsubgroup/salary/api/dto/UserDto.java
@@ -0,0 +1,16 @@
+package edu.strongsubgroup.salary.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private String login;
+    private String password;
+}
